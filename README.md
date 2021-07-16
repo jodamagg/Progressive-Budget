5fe16f5870adbc6daf4f5cb42ac281ad541e159b
unknown <jodamagg@gmail.com>
2021-07-15 20:39:16 -0600

    re added my models and routes

---
+++ README.md
@@ -0,0 +1,8 @@
+Objective of this project was to refactor existing code to provide functionality for offline transactions and rendering. Solution involves deployment of a service worker as well as implementation of IndexedDB.
+
+Deployed App Link: https://infinite-tundra-93440.herokuapp.com/
+
+Preview Screenshot:
+
+![image](https://user-images.githubusercontent.com/34629779/122688559-352ab880-d1da-11eb-997c-6c24349653ed.png)
+
---
+++ public/service-worker.js
@@ -0,0 +1,77 @@
+console.log("I see you, you naughty service worker you!");
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//caching all files in public directory for offline use
+const FILES_TO_CACHE = [
+  "/",
+  "index.js",
+  "index.html",
+  "manifest.webmanifest",
+  "styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+// install SW event
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  //implement SW immediately
+  self.skipWaiting();
+});
+
+// activate SW and remove old cache data
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Listen for fetch event and clone
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
